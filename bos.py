f30d06e71a68b555e1e59a399329f680014496d4
reedbr <reed.br@husky.neu.edu>
2016-12-10 11:02:21 -0500

    Changed again

--- bos.py
+++ bos.py
@@ -87,7 +87,7 @@ def alejandro():
 def doug():
     return render_template('doug.html')
 
-@app.route('/rest_true.csv')
+@app.route('/static/rest_true.csv')
 def rest_true():
     return app.send_static_file('rest_true.csv')
 
