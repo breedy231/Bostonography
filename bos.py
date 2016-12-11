23619af87a3eaaaf9a5657e75de5a653dfdc392a
reedbr <reed.br@husky.neu.edu>
2016-12-11 13:25:41 -0500

    Changed markers

--- bos.py
+++ bos.py
@@ -5,7 +5,6 @@ from flask import Flask, request, session, g, redirect, url_for, abort, \
     render_template, flash, jsonify
 from flask_navigation import Navigation
 
-
 # create our little application :)
 app = Flask(__name__)
 app.config.from_object(__name__)
